b99a1f15bc4c8dab8657f1afc784b63c3c1c745d
Ron Nahmias <ron.nahmias2@gmail.com>
2024-03-06 14:03:45 +0200

    fix port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   // Start the application Port
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
